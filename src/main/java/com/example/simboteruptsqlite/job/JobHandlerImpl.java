34fe1f86e5e1184ad20167e8b63ee484b58dde99
Administrator <2237099022@qq.com>
2021-11-28 23:57:06 +0800

    Initial commit2

--- src/main/java/com/example/simboteruptsqlite/job/JobHandlerImpl.java
+++ src/main/java/com/example/simboteruptsqlite/job/JobHandlerImpl.java
@@ -10,7 +10,7 @@ import xyz.erupt.job.handler.EruptJobHandler;
 @Service
 @EruptHandlerNaming("JobHandlerImpl")  // 如果不添加此配置，类名会作为前端展示依据
 public class JobHandlerImpl implements EruptJobHandler {
-    
+
     /**
      * @param code 任务编码
      * @param param 任务参数
@@ -19,7 +19,7 @@ public class JobHandlerImpl implements EruptJobHandler {
     public String exec(String code, String param) {
         BotManager botManager = RunSuccess.getBotManager();
         botManager.getBot(botManager.getBots().get(0)).getSender().SENDER.sendPrivateMsg("3183740142","启动成功111");
-        
+
         // TODO
         return null;
     }
